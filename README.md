dbd1a2e5a2cc2f0ef648837a987f16a66e2795f9
Daniel Paz <danielphdez@outlook.com>
2025-03-28 16:12:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# GitHub User Activity
+
+This project analyzes GitHub user activity and provides insights into repositories, commits, and contributions.
+
+## Features
+- Analyze user repositories and contributions.
+- Generate activity reports.
+- Visualize data trends.
+
+## Installation
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/your-username/github-user-activity.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd github-user-activity
+   ```
+3. Install dependencies:
+   ```bash
+   npm install
+   ```
+
+## Usage
+1. Run the application:
+   ```bash
+   github-activity <username>
+   ```
+2. Follow the prompts to analyze GitHub user activity.
---
+++ src/action.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+
+program.name('CLI Github Activity')
+    .description('CLI Program to get user activity in Github')
+    .version('1.0.0')
+    .argument('<username>', 'Username')
+    .action( async (username) => {
+       try {
+        const response = await fetch(`https://api.github.com/users/${username}/events`);    
+        if( response.status == 200) {
+            userActivity = await response.json();
+            userActivity.map( (elt) => {
+                console.log(`Pushed 3 commits to ${elt.repo.name}`);
+            })
+        } else {
+            console.log('Wrong status: ', response.status)
+        }
+       } catch( error ) {
+        console.log('New Error: ', error.message)
+       }
+
+    });
+program.parse(process.argv);
